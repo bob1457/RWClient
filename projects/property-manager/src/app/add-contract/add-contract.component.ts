56fdce8bc576950b41ba2cca440e252e36ec21a1
Bob Yuan <bob.yuan@yahoo.com>
2020-05-16 22:28:02 -0700

    UI add property owner

--- projects/property-manager/src/app/add-contract/add-contract.component.ts
+++ projects/property-manager/src/app/add-contract/add-contract.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Property } from '@lib/app-core';
+import { PropertyState } from '../store/property.state';
+import { Store, select } from '@ngrx/store';
+import { propertyList } from '../store/reducers';
 
 @Component({
   selector: 'app-add-contract',
@@ -11,13 +14,14 @@ export class AddContractComponent implements OnInit {
 
   addForm: FormGroup;
 
-  // properties: Property[];
-  properties: any = [
-    { id: '1', propertyName: '621 Coquitlam'},
-    { id: '2', propertyName: '1307 Surrey'}
-  ];
+  properties: Property[];
+  // properties: any = [
+  //   { id: '1', propertyName: '621 Coquitlam'},
+  //   { id: '2', propertyName: '1307 Surrey'}
+  // ];
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder,
+              private store: Store<PropertyState>) { }
 
   ngOnInit() {
 
@@ -26,17 +30,26 @@ export class AddContractComponent implements OnInit {
       managementContractType: ['New'],
       startDate: [''],
       endDate: [''],
+      propertyId: [],
       placementFeeScale: [''],
       managementFeeScale: [''],
       signDate: [''],
       notes: ['']
-
-
     });
+
+    this.store.pipe(select(propertyList))
+    .subscribe(data => this.properties = data);
   }
 
-  onChange(e) {
+  onChange(id) {
+    console.log(id);
+    this.addForm.get('propertyId').setValue(id);
+    console.log(this.addForm.get('propertyId').value);
 
+    // this.store.pipe(select(propertyList))
+    //     .subscribe(res => {
+    //       const property = this.properties.filter( p => p.id === id);
+    //     });
   }
 
 }
--- projects/property-manager/src/app/add-owner/add-owner.component.ts
+++ projects/property-manager/src/app/add-owner/add-owner.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { PropertyState } from '../store/property.state';
+import { Store, select } from '@ngrx/store';
+import { Property, PropertyOwner } from '@lib/app-core';
+import { propertyList, ownerList } from '../store/reducers';
 
 @Component({
   selector: 'app-add-owner',
@@ -7,9 +12,77 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddOwnerComponent implements OnInit {
 
-  constructor() { }
+  addForm: FormGroup;
+
+  ownerOption = 'existing';
+  owners: PropertyOwner[];
+
+  properties: Property[];
+  // properties: any = [
+  //   { id: '1', propertyName: '621 Coquitlam'},
+  //   { id: '2', propertyName: '1307 Surrey'}
+  // ];
+  address: any = {
+    street: '',
+    city: '',
+    provState: '',
+    postZipCoe: '',
+    country: ''
+  };
+
+  selected = false;
+
+
+  constructor(private formBuilder: FormBuilder,
+              private store: Store<PropertyState>) { }
 
   ngOnInit() {
+    this.addForm = this.formBuilder.group({
+      firstName: [],
+      lastName: [],
+      contactEmail: [],
+      contactTelephone1: [],
+      contactTelephone2: [],
+      propertyId: [],
+
+      ownerOption: []
+
+    });
+
+    this.store.pipe(select(propertyList))
+        .subscribe(data => this.properties = data);
+
+    this.store.pipe(select(ownerList))
+        .subscribe(data => {
+          this.owners = data;
+          console.log(this.owners);
+        });
+  }
+
+  onChange(id) { // aelect property
+    this.selected = true;
+    console.log(id);
+    this.addForm.get('propertyId').setValue(id);
+    console.log(this.addForm.get('propertyId').value);
+
+// debugger;
+
+    this.store.pipe(select(propertyList))
+        .subscribe(res => {
+          const property = this.properties.find(p => p.id === id);
+          this.address.street = property.propertyNumber + ' ' + property.propertyStreet;
+          this.address.city = property.propertyCity;
+          this.address.provState = property.propertyStateProvince;
+          this.address.postZipCode = property.propertyZipPostCode;
+          this.address.country = property.propertyCountry;
+
+
+          console.log(this.address);
+        });
+  }
+
+  onOwnerChange(id) {
+
   }
 
 }
