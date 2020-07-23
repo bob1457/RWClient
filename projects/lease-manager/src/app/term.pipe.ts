2fc6f55d4e3bfdae6fa62b753a74b6f003af7823
Bob Yuan <bob.yuan@yahoo.com>
2020-07-23 14:29:05 -0700

    Bug fix

--- projects/lease-manager/src/app/term.pipe.ts
+++ projects/lease-manager/src/app/term.pipe.ts
@@ -6,7 +6,36 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class TermPipe implements PipeTransform {
 
   transform(value: any, args?: any): any {
-    return null;
+
+    switch (value) {
+      case 0 : {
+        return 'NotSet';
+        break;
+      }
+      case 1 : {
+        return 'One Year';
+        break;
+      }
+      case 2 : {
+        return 'Three Month';
+        break;
+      }
+      case 3 : {
+        return 'Six Month';
+        break;
+      }
+      case 4 : {
+        return 'Others';
+        break;
+      }
+      default : {
+        return 'NotSet';
+        break;
+      }
+    }
+
+
+    // return null;
   }
 
 }
