0eb9dca8472b4b3bc5d44b5eb552cdf8282f500d
Bob Yuan <bob.yuan@yahoo.com>
2021-01-24 14:30:59 -0800

    Add addtional schedule of agreement view

--- projects/lease-manager/src/app/agreement-party-view/agreement-party-view.component.spec.ts
+++ projects/lease-manager/src/app/agreement-party-view/agreement-party-view.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { AgreementViewComponent } from './agreement-view.component';
+import { AgreementViewComponent } from './agreement-party-view.component';
 
 describe('AgreementViewComponent', () => {
   let component: AgreementViewComponent;
