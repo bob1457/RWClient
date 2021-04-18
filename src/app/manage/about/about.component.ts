a3d5bb7990b562ffa06e49d2c464d4154d936a88
Bob Yuan <bob.yuan@yahoo.com>
2021-04-17 22:28:13 -0700

    Add about page design

--- src/app/manage/about/about.component.ts
+++ src/app/manage/about/about.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
+  tabs = [
+    {tab: 'System Info'},
+    {tab: 'Support Info'},
+    {tab: 'EULA'}
+  ];
   constructor() { }
 
   ngOnInit() {
