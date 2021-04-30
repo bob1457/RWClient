792f40aacdd54d53652a2bde78eb93cbccf90ff7
Bob Yuan <bob.yuan@yahoo.com>
2021-04-30 14:41:42 -0700

    Update prod env variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,20 @@
+import * as packageJson from '../../package.json' ;
+
 export const environment = {
   version: '0.9.0',
+  appName: 'REALWorks',
+  appVersion: '0.8.0(Beta 1)',
+  appDescription: 'Realestate property managememt system',
   production: true,
   weatherApiKey: '3396569b4df442909e0203014190809', // Apixu weather api
-  weatherBaseUri: ''
+  weatherBaseUri: '',
+  versions: {
+    app: packageJson.version,
+    angular: packageJson.dependencies['@angular/core'],
+    angularCli: packageJson.devDependencies['@angular/cli'],
+    ngrx: packageJson.dependencies['@ngrx/store'],
+    rxjs: packageJson.dependencies.rxjs,
+    typescript: packageJson.devDependencies['typescript'],
+    material: packageJson.dependencies['@angular/material']
+  }
 };
