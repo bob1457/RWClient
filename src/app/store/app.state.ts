e1a87ece5f78a5a1a30e53d1767294c1cd7f10e2
Bob Yuan <bob.yuan@yahoo.com>
2021-05-25 17:21:44 -0700

    Fixed error on root state

--- src/app/store/app.state.ts
+++ src/app/store/app.state.ts
@@ -3,12 +3,12 @@
 import { RouterState } from './router.reducer';
 import * as fromAuth from '@lib/auth'; // ../../../projects/auth/src/lib/store/auth.state';
 import * as fromProperty from '../../../projects/property-manager/src/app/store/property.state';
-import { p_reducer } from '../../../projects/property-manager/src/app/store/reducers';
+// import {  } from '../../../projects/property-manager/src/app/store/reducers';
 import * as fromDash from '@lib/dashboard';
 import * as fromMarketing from '../../../projects/marketing-manager/src/app/store/marketing.state';
 import * as fromLease from '../../../projects/lease-manager/src/app/store/lease-state';
 
-import { PROPERTY_STATE_NAME } from '../../../projects/property-manager/src/app/store/reducers';
+import { PROPERTY_STATE_NAME, p_reducer} from '../../../projects/property-manager/src/app/store/reducers';
 import { MARKETING_STATE_NAME, m_reducer } from 'projects/marketing-manager/src/app/store/reducers';
 import { LEASE_STATE_NAME, l_reducer } from 'projects/lease-manager/src/app/store/reducers';
 
@@ -25,7 +25,7 @@ export const appReducer = {
   // [SHARED_STATE_NAME]: SharedReducer,
   // [AUTH_STATE_NAME]: AuthReducer,
   // router: routerReducer,
-  [PROPERTY_STATE_NAME]: p_reducer,
-  [MARKETING_STATE_NAME]: m_reducer,
-  [LEASE_STATE_NAME]: l_reducer
+  PROPERTY_STATE_NAME: p_reducer,
+  MARKETING_STATE_NAME: m_reducer,
+  LEASE_STATE_NAME: l_reducer
 };
