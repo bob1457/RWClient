73f88db38d3ff332a0a8b80796b855a73aa3a36d
Bob Yuan <bob.yuan@yahoo.com>
2021-06-16 16:46:06 -0700

    Refactor owner adding

--- projects/property-manager/src/app/add-owner/add-owner.component.ts
+++ projects/property-manager/src/app/add-owner/add-owner.component.ts
@@ -45,6 +45,8 @@ export class AddOwnerComponent implements OnInit {
 
   emailExists = false;
 
+  loading = false;
+
   constructor(private formBuilder: FormBuilder,
               private propertyService: PropertyService,
               private location: Location,
@@ -116,10 +118,10 @@ export class AddOwnerComponent implements OnInit {
     .subscribe(res => {
       const owner = this.owners.find(o => o.id === id); // const owner: any = this.owners.find(o => o.id === id);
       this.addForm.get('propertyOwnerId').setValue(owner.id);
-      this.addForm.get('firstName').setValue(owner.firstName);
-      this.addForm.get('lastName').setValue(owner.lastName);
-      this.addForm.get('contactEmail').setValue(owner.contactEmail);
-      this.addForm.get('contactTelephone1').setValue(owner.contactTelephone1);
+      // this.addForm.get('firstName').setValue(owner.firstName);
+      // this.addForm.get('lastName').setValue(owner.lastName);
+      // this.addForm.get('contactEmail').setValue(owner.contactEmail);
+      // this.addForm.get('contactTelephone1').setValue(owner.contactTelephone1);
       // this.ownerAddress.street = owner.streetNumber;
       // this.ownerAddress.city = owner.city;
       // this.ownerAddress.provState = owner.stateProv;
@@ -142,32 +144,64 @@ export class AddOwnerComponent implements OnInit {
       this.addForm.get('onlineAccessEnabled').setValue(false);
       this.addForm.get('userName').setValue('NotSet');
       this.addForm.get('roleId').setValue(2);
+
+      this.propertyService.checUserkEmail(this.addForm.controls['contactEmail'].value)
+          .subscribe(res => {
+            let emailexists;
+            emailexists = res;
+            console.log('email check result', emailexists);
+
+            if (emailexists) {
+              // alert(this.addForm.controls['contactEmail'].value + ' already exists!');
+              this.openSnackBar(this.addForm.controls['contactEmail'].value + ' already exists!', 'dismiss', 'error');
+            } else {
+              this.store.dispatch(PropertyActions.addPropertyOwner({payload: this.addForm.value})); // disabled for testing
+              // alert('ok, go ahead to create owner');
+              this.location.back();
+            }
+
+          });
+
+    } else {
+      // call service directly to add existing owenr
+      try{
+        debugger;
+
+        this.loading = true;
+
+        this.propertyService.addOwner(this.addForm.value)
+              .subscribe((owner) => {
+                this.loading = false;
+                console.log('owner added', owner);
+              });
+        // console.log('add owner form', this.addForm.value);
+        this.openSnackBar('Owner added successfully!', 'close', 'notify');
+        this.location.back();
+      } catch(err) {
+        this.openSnackBar(err.message, 'dismiss', 'error');
+      }
     }
     // console.log(formValue);
 
 
     // Check if the email already exists
 
-    // this.uerEmailExists( this.addForm.controls['contactEmail'].value );
+    // this.propertyService.checUserkEmail(this.addForm.controls['contactEmail'].value)
+    // .subscribe(res => {
+    //   let emailexists;
+    //   emailexists = res;
+    //   console.log('email check result', emailexists);
 
+    //   if (emailexists) {
+    //     // alert(this.addForm.controls['contactEmail'].value + ' already exists!');
+    //     this.openSnackBar(this.addForm.controls['contactEmail'].value + ' already exists!', 'dismiss', 'error');
+    //   } else {
+    //     this.store.dispatch(PropertyActions.addPropertyOwner({payload: this.addForm.value})); // disabled for testing
+    //     // alert('ok, go ahead to create owner');
+    //     this.location.back();
+    //   }
 
-
-    this.propertyService.checUserkEmail(this.addForm.controls['contactEmail'].value)
-    .subscribe(res => {
-      let emailexists;
-      emailexists = res;
-      console.log('email check result', emailexists);
-
-      if (emailexists) {
-        alert(this.addForm.controls['contactEmail'].value + ' already exists!');
-        this.openSnackBar(this.addForm.controls['contactEmail'].value + ' already exists!', 'dismiss', 'error');
-      } else {
-        this.store.dispatch(PropertyActions.addPropertyOwner({payload: this.addForm.value})); // disabled for testing
-        // alert('ok, go ahead to create owner');
-        this.location.back();
-      }
-
-    });
+    // });
 
   }
 
