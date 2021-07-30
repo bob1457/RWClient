006456045bc7e7b25f646d46acfd1c74e7814986
Bob Yuan <bob.yuan@yahoo.com>
2021-07-30 16:54:32 -0700

    Add data binding to mutual agreemetn view

--- projects/lease-manager/src/app/mutual-agreement/mutual-agreement.component.ts
+++ projects/lease-manager/src/app/mutual-agreement/mutual-agreement.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-mutual-agreement',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mutual-agreement.component.scss']
 })
 export class MutualAgreementComponent implements OnInit {
+
+  @Input() agreement;
 
   constructor() { }
 
