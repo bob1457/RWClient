e9cbd9366080d37c4b3f009c4fb4589b0631bbb9
Bob Yuan <bob.yuan@yahoo.com>
2021-09-30 17:48:02 -0700

    Format addendum PDF view and download

--- projects/lease-manager/src/app/dialogs/addendum-details-dialgo/addendum-details-dialgo.component.ts
+++ projects/lease-manager/src/app/dialogs/addendum-details-dialgo/addendum-details-dialgo.component.ts
@@ -1,8 +1,12 @@
 import { Component, Inject, OnInit, Optional } from '@angular/core';
-import { MAT_DIALOG_DATA } from '@angular/material';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { select, Store } from '@ngrx/store';
 import { getUserInfo } from '@lib/auth';
 import { PropertyState } from 'projects/property-manager/src/app/store/property.state';
+
+import * as jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
+import * as html2pdf from 'html2pdf.js';
 
 @Component({
   selector: 'app-addendum-details-dialgo',
@@ -14,6 +18,7 @@ export class AddendumDetailsDialgoComponent implements OnInit {
   user;
 
   constructor(private store: Store<PropertyState>,
+              public dialogRef: MatDialogRef<AddendumDetailsDialgoComponent>,
               @Optional() @Inject(MAT_DIALOG_DATA) public data: { addendum: any }) {
 
     this.store.pipe(select(getUserInfo))
@@ -31,6 +36,27 @@ export class AddendumDetailsDialgoComponent implements OnInit {
   ngOnInit() {
 
     console.log('addendum in dialog', this.data.addendum);
+  }
+
+  download() {
+    debugger;
+
+    const element = document.getElementById('pdfdoc');
+
+    const options = {
+      margin: 0.2,
+      // tslint:disable-next-line:max-line-length
+      filename: 'Rental Agreement_' + this.data.addendum.leaseId +  '_Addendum', // this.contract.managementContractTitle + '_' + timestamp + '_contract.pdf',
+      image: { type: 'jpeg', quality: 0.98 },
+      html2canvas: { scale: 1 },
+      jsPDF: { unit: 'in', format: 'a4', orientation: 'portrait' }
+    };
+
+    html2pdf().from(element).set(options).save();
+  }
+
+  Ok() {
+    this.dialogRef.close();
   }
 
 }
